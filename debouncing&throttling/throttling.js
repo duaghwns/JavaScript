3db25618ced36d8e06f64d133579ddcfa4325436
duaghwns <hj2758@gmail.com>
2022-02-23 15:51:12 +0900

    Help me

--- debouncing&throttling/throttling.js
+++ debouncing&throttling/throttling.js
@@ -3,6 +3,8 @@ const $normal = document.querySelector('#normal');
 const $throttled = document.querySelector('#throttling');
 
 console.log($scrollHere)
+console.log($normal)
+console.log($throttled)
 
 const throttle = (callback, delay) => {
     let timer;
@@ -12,8 +14,8 @@ const throttle = (callback, delay) => {
             callback(event);
             timer = null;
         }, delay, event);
-    }
-}
+    };
+};
 
 let Count = { normalCount:0, throttlingCount:0};
 
@@ -22,7 +24,16 @@ $scrollHere.addEventListener('scroll', ()=>{
     $normal.textContent = ++Count.normalCount;
 });
 
+const colr = ['red','yellow','green','blue','blueviolet'];
+let cnt = 0;
 $scrollHere.addEventListener('scroll', throttle(()=>{
     console.log(123)
     $throttled.textContent = ++Count.throttlingCount;
+    
+    if(Count.throttlingCount % 10==0){
+        $scrollHere.style.backgroundColor = colr[cnt++];
+    }
+    if(cnt==4){
+        cnt = 0;
+    }
 },100));
