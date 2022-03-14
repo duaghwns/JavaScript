5488e5df2d7d80e5e0f28b0b250ae4123264d71a
duaghwns <hj2758@gmail.com>
2022-03-14 14:06:27 +0900

    df

--- js/fileupload.js
+++ js/fileupload.js
@@ -1,10 +1,15 @@
 const box = document.querySelector('.box');
 const file = document.querySelector(`#file`)
 const files = file.files;
-
-console.log(files)
-
-handleChangeFile = e => {
+const handleChangeFile = e => {
     e.preventDefault();
     console.log(e)
+    file.click();
+    console.log(file.files)
 }
+
+const span = document.querySelector('.box > span');
+console.log(span)
+
+box.addEventListener('click', handleChangeFile);
+
