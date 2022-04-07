63476c30990e4b05082ccf27e51fc58ffca88817
duaghwns <hj2758@gmail.com>
2022-04-08 01:16:56 +0900

    menubar

--- menubar/menubar1.js
+++ menubar/menubar1.js
@@ -4,10 +4,10 @@ const tabcon = document.querySelectorAll("[data-tab-content]");
 
 tabs.forEach((tab)=> {
     tab.addEventListener('click',()=>{
-        const target = document.querySelector(tab.dataset.tabTargets);
+        const target = document.querySelector(tab.dataset.tabTarget);
         tabcon.forEach((tabc_all)=>{
             tabc_all.classList.remove('active');
         });
-        tabc_all.classList.add('active');
+        target.classList.add('active');
     });
-})
+});
