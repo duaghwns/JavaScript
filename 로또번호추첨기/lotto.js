5e8a90010572c3847b46e4a679714b74e64deb03
duaghwns <hj2758@gmail.com>
2022-04-10 14:38:40 +0900

    lotto

--- 로또번호추첨기/lotto.js
+++ 로또번호추첨기/lotto.js
@@ -1,5 +1,8 @@
 const autoButton = document.querySelector('#lotto');
 const numBox = document.querySelectorAll('.lotto');
+const numbers = document.querySelectorAll('.numbers');
+const choiceNum = document.querySelectorAll('.choiceNum');
+
 
 const makeNumbers = () => {
     let array = new Array();
@@ -16,9 +19,24 @@ const makeNumbers = () => {
 
 const excute = () => {
     const array = makeNumbers();
-    numBox.forEach((i,j,k,n)=>{
+    numBox.forEach((i,j)=>{
         i.innerHTML = array[j];
     });
 }
 
+
+numbers.forEach(num => {
+    let cnt = 0;
+    num.addEventListener('click',()=>{
+        num.classList.add('active');
+        choiceNum[cnt].value = num.innerHTML;
+        console.log(num.innerHTML)
+        console.log(choiceNum[cnt])
+        const active = document.querySelector('.active');
+        active.addEventListener('click',()=>{
+            active.classList.remove('active');
+        });
+    });
+})
 autoButton.addEventListener('click',excute);
+
